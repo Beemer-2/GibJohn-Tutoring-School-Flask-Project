48b315befaa7c412c5e9c4d87bb229f2b52f681a
Brandon White <beemski2007@gmail.com>
2024-11-11 16:49:23 +0000

    added more to pages and the backend

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 import sqlite3
+import datetime
+import hashlib
+    
 
 app = Flask(__name__)
 
@@ -16,12 +19,39 @@ def log_in_sign_up():
     return render_template("log-in-sign-up-page.html")
 
 @app.route("/sign-up", methods=["POST"])
+
 def create_account():
-    data = request.form
-    print(data)
-    database = sqlite3.connect("database/main.db")
-    
-    return "account created with details"
+    if request.method == "POST":
+        data = request.form
+        database = sqlite3.connect("database/main.db")
+        db_cursor = database.cursor()
+
+        date = datetime.datetime.now()
+
+        email = data.get("email")
+        if "@" and "." not in email:
+            return "error, invalid email"
+
+        phoneNum = data.get("phoneNum")
+        print(phoneNum)
+        try:
+            int(phoneNum)
+        except:
+            return "error, invalid phone number"
+
+        current_ID = db_cursor.execute("SELECT ID FROM users WHERE ID=(SELECT max(ID) FROM users)").fetchall()[0][0] + 1
+
+
+        db_cursor.execute("""INSERT INTO users 
+                      (username, password, ID, email, phoneNum, creationDate, lessonsOwned) 
+                      VALUES (?,?,?,?,?,?,?)"""
+                      , (data.get("username"), hashlib.sha512(data.get("password").encode()).hexdigest(), current_ID, email, phoneNum, date, 0))
+
+
+        database.commit()
+        database.close()
+
+        return "account created with details"
 
 @app.route("/log-in", methods=["POST"])
 def log_in_to_account():
@@ -34,3 +64,7 @@ def log_in_to_account():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
--- initialise-database.py
+++ initialise-database.py
@@ -1,7 +1,34 @@
 import sqlite3
 
+
+#database = sqlite3.connect("database/main.db")
+#db_cursor = database.cursor()
+#db_cursor.execute("CREATE TABLE users (username VARCHAR UNIQUE, password VARCHAR, ID INT UNIQUE, email VARCHAR, phoneNum INT, creationDate DATETIME, lessonsOwned VARCHAR)")
+#database.commit()
+#database.close()
+
 database = sqlite3.connect("database/main.db")
 db_cursor = database.cursor()
-db_cursor.execute("CREATE TABLE users (username VARCHAR, password VARCHAR, ID INT, email VARCHAR, phoneNum INT, creationDate DATETIME, lessonsOwned VARCHAR)")
+
+#db_cursor.execute("SELECT ID FROM users WHERE ID=(SELECT max(ID) FROM users);")
+
+import datetime
+
+date = datetime.datetime.now()
+
+db_cursor.execute("""INSERT INTO users 
+                  (username, password, ID, email, phoneNum, creationDate, lessonsOwned) 
+                  VALUES (?,?,?,?,?,?,?)"""
+                 , ("Bob", "thisIsATest", 1, "abc@example.com", 123456789, date, 0))
+print(db_cursor.fetchall())
 database.commit()
 database.close()
+#
+#
+
+database = sqlite3.connect("database/main.db")
+db_cursor = database.cursor()
+
+db_cursor.execute("SELECT * FROM users")
+
+print(db_cursor.fetchall())
