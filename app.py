fa5313e31320bc3d469a5f3c6c10db2a4fecf2d9
Beemer-2 <151047145+Beemer-2@users.noreply.github.com>
2024-11-17 15:35:02 +0000

    Added error pages and worked on responsiveness

--- app.py
+++ app.py
@@ -18,98 +18,130 @@ Session(app)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    print("username")
+    return render_template("home.html", username = session.get("username"))
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", username = session.get("username"))
 
 @app.route("/log-in-sign-up-page")
 def log_in_sign_up():
-    return render_template("log-in-sign-up-page.html")
+    return render_template("log-in-sign-up-page.html", username = session.get("username"))
 
 @app.route("/sign-up", methods=["POST"])
 def create_account():
-    data = request.form
-
-    #Sets up and opens the database
-    database = sqlite3.connect("database/main.db")
-    db_cursor = database.cursor()
-    
-    date = datetime.datetime.now()
-    
-    email = data.get("email")
-    #Checks and makes sure there is a "@" and a "." in the email entered
-    if "@" and "." not in email:
-        return "error, invalid email"
-    
-    #Ensures the number entered is a number and nothing else
-    phoneNum = data.get("phoneNum")
-    print(phoneNum)
     try:
-        int(phoneNum)
-    except:
-        return "error, invalid phone number"
+        data = request.form
+
+        if data.get("username") == "":
+            return render_template("invalid-sign-up-username.html")
+
+
+        #Sets up and opens the database
+        database = sqlite3.connect("database/main.db")
+        db_cursor = database.cursor()
+
+        date = datetime.datetime.now()
+
+        email = data.get("email")
+        #Checks and makes sure there is a "@" and a "." in the email entered
+        if "@" and "." not in email:
+            return render_template("invalid-email.html")
+
+        #Ensures the number entered is a number and nothing else
+        phoneNum = data.get("phoneNum")
+        print(phoneNum)
+        try:
+            int(phoneNum)
+        except:
+            return render_template("invalid-phone-number.html")
+
+        #Gets the last user entered into the database's ID
+        current_ID = db_cursor.execute("SELECT ID FROM users WHERE ID=(SELECT max(ID) FROM users);").fetchall()[0][0] + 1
+        print(current_ID)
+
+        password_len = len(data.get("password"))
+
+        if password_len >= 8:
+            pass
+        else:
+            return render_template("invalid-sign-up-password.html")
+
+        #Generates the salt
+        salt = bcrypt.gensalt()
+        print(salt)
+
+        password = bcrypt.hashpw(data.get("password").encode("utf-8"), salt)
+
+        #Inserts the entered data into the database as a new user. Generates a hash for the password in the process.
+        db_cursor.execute("""INSERT INTO users 
+                      (username, password, ID, email, phoneNum, creationDate, lessonsOwned) 
+                      VALUES (?,?,?,?,?,?,?)"""
+                      , (data.get("username"), password, current_ID, email, phoneNum, date, 0))
+
+
+        #Saves the changes
+        database.commit()
+        #Closes the database
+        db_cursor.close()
+        database.close()
+
+        session["username"] = data.get("username")
+
+        return render_template("home.html", username = session.get("username"))
     
-    current_ID = db_cursor.execute("SELECT ID FROM users WHERE ID=(SELECT max(ID) FROM users);").fetchall()[0][0] + 1
-    print(current_ID)
+    except:
+        return render_template("unknown-sign-up-error.html")
 
-    password_len = len(data.get("password"))
-
-    if password_len >= 8:
-        pass
-    else:
-        return "Password is too short or too long. It needs to be between 8 and 128 characters."
-
-    #Generates the salt
-    salt = bcrypt.gensalt()
-    print(salt)
-
-    password = bcrypt.hashpw(data.get("password").encode("utf-8"), salt)
-
-    #Inserts the entered data into the database as a new user. Generates a hash for the password in the process.
-    db_cursor.execute("""INSERT INTO users 
-                  (username, password, ID, email, phoneNum, creationDate, lessonsOwned) 
-                  VALUES (?,?,?,?,?,?,?)"""
-                  , (data.get("username"), password, current_ID, email, phoneNum, date, 0))
-
-
-    #Saves the changes
-    database.commit()
-    #Closes the database
-    database.close()
-
-    session["username"] = data.get("username")
-
-    return render_template("templates/index.html", username = session.get("username"))
 
 @app.route("/log-in", methods=["POST"])
 def log_in_to_account():
-    
-    data = request.form
-    
-    database = sqlite3.connect("database/main.db")
-    db_cursor = database.cursor()
+    try:
+        data = request.form
 
-    username = data.get("log-in-username")
-    
-    attempted_log_into = db_cursor.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchall()
+        #Loads the database
+        database = sqlite3.connect("database/main.db")
+        db_cursor = database.cursor()
 
-    if attempted_log_into != []:
-        entered_password = data.get("log-in-password").encode()
-        password_stored = db_cursor.execute("SELECT password FROM users WHERE username = ?", (username,)).fetchall()[0][0]
+        username = data.get("log-in-username")
 
-        if bcrypt.checkpw(entered_password, password_stored):
-            return render_template("templates/index.html", username = session.get("username"))
-        
+        #Gets the account which matches the username the user is attempting to log in to
+        attempted_log_into = db_cursor.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchall()
+
+        #Checks if the account exists
+        if attempted_log_into != []:
+            #If it does, get the password and check to see if the password entered and the password stored match
+            entered_password = data.get("log-in-password").encode()
+            password_stored = db_cursor.execute("SELECT password FROM users WHERE username = ?", (username,)).fetchall()[0][0]
+
+            db_cursor.close()
+            database.close()
+
+            #If the passwords match, log the user in
+            if bcrypt.checkpw(entered_password, password_stored):
+                session["username"] = username
+                print(session["username"])
+                return render_template("home.html", username = session.get("username"))
+
+            else:
+                return render_template("invalid-password.html")
+
         else:
-            return "invalid password!"
+            db_cursor.close()
+            database.close()
 
-    else:
-        return "invalid"
+            return render_template("invalid-username.html")
     
-
-
+    except:
+        render_template("unknown-log-in-error.html")
+    
+@app.route("/log-out")
+def log_out():
+    #Clears the sessions, logging the user out of their account
+    print(session.get("username"))
+    session.clear()
+    return render_template("logged-out.html")
 
     
     
